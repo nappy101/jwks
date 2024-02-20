12bb5fbfb3f33e6dc38d28c8e6910557e3be6b45
Nathaly Pineda <90016018+nappy101@users.noreply.github.com>
2024-02-20 15:38:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,72 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa 
 import jwt
+import uuid
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
-  
 
-#generate key pair and jwk
-def gen_key():
-    #private key
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-  )
+# private key
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+# public key
+public_key = private_key.public_key()
+
+#kid
+kid = str(uuid.uuid1())
+
+# expiration
+exp_time = datetime.utcnow() + timedelta(days=365)
+
+#jwks dictionary encoding
+jwks = {
+    "keys": [
+        {
+        "kty": "RSA",
+        "kid": kid,
+        "use": "sig",
+        "alg": "RS256",
+        "n": public_key.public_numbers().n,
+        "e": public_key.public_numbers().e,
+        "exp": int(exp_time.timestamp()),
+        }
+    ]
+}
+
+# generate JWT
+@app.route('/auth', methods=['POST'])
+def gen_jwt():
+    # expiration
+    exp_time = datetime.utcnow() + timedelta(minutes=2)
+    expired = request.args.get('expired', '').lower() == 'true'
+
+    payload = {'kid': kid, 'exp': int(exp_time.timestamp())}
+    header = {"alg": "RS256", "typ": "JWT", "kid": kid} 
     
-  #public key
-  public_key = private_key.public_key()
+    try:
+        token = jwt.encode(payload, private_key, algorithm='RS256', headers=header)
+        return jsonify({'JWT token': token})
+        
+    except jwt.ExpiredSignatureError:
+        return jsonify({'ERROR': 'Expired JWT'}), 401
 
-  #
-  
-  #expiration set for 1 year
-  exp_time = datetime.datetime.utcnow() + datetime.timedelta(days=365)
-  
-  #encode jwk
-  
-  
-  #/auth POST
-  
-  
-  #/.well-known/jwks.json GET
+
+# JWKS Endpoint - this serves public keys
+@app.route('/.well-known/jwks.json', methods=['GET'])
+def get_jwks():
+    if jwks_list:
+        response =  jsonify(jwks_list[0])
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'
+        return response
+    else:
+        return jsonify({'ERROR': 'JWKS not found'}), 404
 
 
 
 if __name__ == '__main__':
-    #run on port 8080
     app.run(port=8080)

dea7bf66ab26c665326d31257819df2e8170895e
Nathaly Pineda <90016018+nappy101@users.noreply.github.com>
2024-03-02 20:10:26 -0600

    Update main.py
    fixed expired token error by generating a new kid in the jwt and appending a new key for each generation

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ private_key = rsa.generate_private_key(
 # get public key
 public_key = private_key.public_key()
 
-
 #*****************************************************
 # public key generation to verify signature on jwt.io
 pub_key_pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
@@ -28,7 +27,7 @@ pub_key_pem = public_key.public_bytes(encoding=serialization.Encoding.PEM,
 loaded_pub_key_pem = serialization.load_pem_public_key(pub_key_pem, backend=default_backend)
 
 # use this in jwt.io to verify signature
-print("Public Key:", pub_key_pem.decode('utf-8'))
+print("\n Public Key:", pub_key_pem.decode('utf-8'))
 #****************************************************
 
 
@@ -50,11 +49,11 @@ jwks_dict = {
     ]
 }
 
-# list
+# Store the JWK in a list
 jwks = [jwks_dict["keys"]]
-print(jwks)
+print("\n JWKS: ", jwks)
 
-# JWT endpoint
+# Endpoint to generate JWT
 @app.route('/auth', methods=['POST'])
 def generate_jwt():
     try:
@@ -62,36 +61,54 @@ def generate_jwt():
         expired = request.args.get('expired', False)
 
         if expired:
-            # expired 20 minutes ago
-            exp_time = datetime.now(timezone.utc) - timedelta(minutes=20)
+            # expired 20 seconds ago
+            exp_time = datetime.now(timezone.utc) - timedelta(seconds=20)
         else:
             # will expire in 10 minutes
             exp_time = datetime.now(timezone.utc) + timedelta(minutes=10)
+
+        # new kid for each jwt
+        kid = str(uuid.uuid4())
+
+        # create a new key and append to jwks_dict
+        if not expired:
+            new_key = {
+                    "kty": "RSA",
+                    "kid": kid,
+                    "use": "sig",
+                    "alg": "RS256",
+                    "n": base64.urlsafe_b64encode(public_key.public_numbers().n.to_bytes(256, 'big')).rstrip(b'=').decode('utf-8'),
+                    "e": "AQAB",
+                    "exp": int(exp_time.timestamp()),
+                }
+            jwks_dict["keys"].append(new_key)
 
         header = {"kid": kid, "alg": "RS256", "typ": "JWT"}
 
         # token creation
         token = jwt.encode({'exp': exp_time.timestamp()}, private_key, algorithm='RS256', headers=header)
-        print("Encoded token: ", token)
+        
+        print("\n Encoded token: ", token, "\n")
         return token, 200
     
     except jwt.ExpiredSignatureError:
-        print("Expired token: ", token)
+        print("Expired token: ", token, "\n")
+        return "Token expired", 401
 
-    except Exception as error_generating_token:
-        print("Error generating token: , {error_generating_token}")
-        return "Error generating token: ", 500
+    except jwt.InvalidTokenError as error_generating_token:
+        print("POST: Error generating token: " , {error_generating_token}, "\n")
+        return "POST: Error generating token: ", 401
 
 
-# JWKs endpoint
+# Endpoint to retrieve JWKs
 @app.route('/.well-known/jwks.json', methods=['GET'])
 def get_jwks():
     try:
         return jsonify(jwks_dict), 200
     
     except Exception as get_jwks_error:
-        print("Error getting JWKs: {get_jwks_error}")
-        return "Error getting JWKs", 500
+        print("GET: Error getting JWKs: ", {get_jwks_error}, "\n")
+        return "GET: Error getting JWKs", 404
 
 
 if __name__ == '__main__':
